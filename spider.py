a709f7216589914a83b2c77d83c8ac4ab594cc85
Xu Han <44578002+hanxu49@users.noreply.github.com>
2023-03-09 12:50:43 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -57,7 +57,7 @@ if not os.path.exists("build"):
 
 session = Session()
 session.headers.update({
-    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x6307061d)",
+    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.33(0x18002120) NetType/WIFI Language/en",
     "Host": "api.issks.com"
 })
 session.cookies.update(dict(
